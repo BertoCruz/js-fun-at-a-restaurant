926a69fabe42913663656b551405037c88c35357
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-07-07 20:47:50 -0600

    Updated meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,33 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+function createMenuItem(itemName, itemPrice, itemType) {
+  return {
+    name : itemName,
+    price : itemPrice,
+    type : itemType
+  };
+}
+
+function addIngredients(topping, ingredients) {
+  if(ingredients.includes(topping)){
+    ingredients.pop();
+  }
+    ingredients[ingredients.length] = topping;
+    console.log(ingredients);
+    return ingredients;
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
